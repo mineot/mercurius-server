c57f32fe404b6cff1f3a6a4b0a9a126021adcac3
Mineot <mineot@gmail.com>
2024-01-23 10:59:25 -0300

    prisma - create crypto and security modules

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { SecurityModule } from './security/security.module';
+import { CryptoModule } from './crypto/crypto.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, SecurityModule, CryptoModule],
 })
 export class CoreModule {}
