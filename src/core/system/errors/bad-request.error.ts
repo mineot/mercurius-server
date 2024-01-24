f3498fdd07fec33af1182b90023225cf97a1b508
Mineot <mineot@gmail.com>
2024-01-24 16:00:58 -0300

    alpha/index - doc bad request error

--- src/core/system/errors/bad-request.error.ts
+++ src/core/system/errors/bad-request.error.ts
@@ -1,18 +1,48 @@
 import { BadRequestException } from '@nestjs/common';
 
+/**
+ * Represents a template error for bad requests.
+ * @interface
+ */
 export interface BadRequestTemplateError {
+  /**
+   * The origin of the error.
+   * @type {string[]}
+   */
   origin: string[];
+  /**
+   * The internationalization reference for the error.
+   * @type {string}
+   */
   i18nRef: string;
+  /**
+   * The error object.
+   * @type {Error}
+   */
   error: Error;
 }
 
+/**
+ * Represents a BadRequestError, abstracting from BadRequestException.
+ */
 export class BadRequestError {
   private $template: BadRequestTemplateError;
 
+  /**
+   * Creates an instance of BadRequestError.
+   * @param {object} options - The options for creating the BadRequestError.
+   * @param {string} options.origin - The origin of the error.
+   * @param {string} options.i18nRef - The internationalization reference of the error.
+   * @param {Error} options.error - The error object.
+   */
   constructor({ origin, i18nRef, error }: BadRequestTemplateError) {
     this.$template = { origin, i18nRef, error };
   }
 
+  /**
+   * Throws a BadRequestException with the values from the $template property.
+   * @throws {BadRequestException}
+   */
   doThrow() {
     throw new BadRequestException({
       origin: this.$template.origin,
