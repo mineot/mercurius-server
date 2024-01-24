4d1fe7db4186cb568508c61b3c12cf58c3294c20
Mineot <mineot@gmail.com>
2024-01-24 14:11:12 -0300

    alpha/index - crypto tests and exception

--- src/core/security/crypto.service.ts
+++ src/core/security/crypto.service.ts
@@ -1,13 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { BadRequestError } from '../errors/badrequest.error';
 
+/**
+ * Service for handling cryptographic operations such as hashing and checking text.
+ */
 @Injectable()
 export class CryptoService {
+  /**
+   * Hashes the input text and returns the hashed string.
+   * @param {string} text - The input text to be hashed.
+   * @returns {Promise<string>} A promise that resolves to the hashed string.
+   * @throws {BadRequestException<BadRequestError>} throws an exception if the text is null or empty.
+   */
   async hash(text: string): Promise<string> {
-    const salt = await bcrypt.genSalt();
-    return await bcrypt.hash(text, salt);
+    try {
+      if (!text) {
+        throw 'text cannot be null or empty';
+      }
+
+      const salt: string = await bcrypt.genSalt();
+      const hashedText: string = await bcrypt.hash(text, salt);
+      return hashedText;
+    } catch (error) {
+      throw new BadRequestException(<BadRequestError>{
+        origin: ['core', 'security', 'crypto.service', 'hash'],
+        i18nRef: 'exception.hashing.failed',
+        message: error.message,
+        error,
+      });
+    }
   }
 
+  /**
+   * Compares the input text with the hashed text using bcrypt.
+   * @param {string} text - The input text to be compared.
+   * @param {string} hashedText - The hashed text to compare with.
+   * @returns {Promise<boolean>} A promise that resolves to a boolean indicating whether the texts match.
+   */
   async check(text: string, hashedText: string): Promise<boolean> {
     return await bcrypt.compare(text, hashedText);
   }
--- test/jest/core/security/crypto.service.spec.ts
+++ test/jest/core/security/crypto.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CryptoService } from '../../../../src/core/security/crypto.service';
 
@@ -12,13 +13,29 @@ describe('CryptoService', () => {
     service = module.get<CryptoService>(CryptoService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+  it('should be defined', () => expect(service).toBeDefined());
 
   it('hash and check', async () => {
     const hashed = await service.hash('123456789');
     const result = await service.check('123456789', hashed);
     expect(result).toBe(true);
+  });
+
+  it('hash text null', async () => {
+    try {
+      await service.hash(null);
+      fail('text cannot be null');
+    } catch (err) {
+      expect(err).toBeInstanceOf(BadRequestException);
+    }
+  });
+
+  it('hash text empty', async () => {
+    try {
+      await service.hash('');
+      fail('text cannot be empty');
+    } catch (err) {
+      expect(err).toBeInstanceOf(BadRequestException);
+    }
   });
 });
