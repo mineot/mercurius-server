25650ea31a9fe86feffb171cab2492313996e79d
Mineot <mineot@gmail.com>
2024-01-24 09:21:35 -0300

    alpha/index - improvement app service test

---
+++ test/jest/app.service.spec.ts
@@ -0,0 +1,18 @@
+import { AppService } from '@/app.service';
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('AppService', () => {
+  let service: AppService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AppService],
+    }).compile();
+
+    service = module.get<AppService>(AppService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
