97c2e0511db7e6ea9ee72258af008006779eb361
Mineot <mineot@gmail.com>
2024-03-04 22:18:15 -0300

    laravel - some tests

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group([], function () {
+    Route::get('/', function () {
+        return  [];
+    });
+
+    Route::get('welcome', function () {
+        return view('welcome');
+    });
 });
