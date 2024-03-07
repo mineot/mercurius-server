182fd7dd7d35ca0b4cf176caccab6a31f1d25d68
Mineot <mineot@gmail.com>
2024-03-07 00:50:14 -0300

    master - fix routes

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([], function () {
     Route::get('/', function () {
-        return  [];
-    });
-
-    Route::get('welcome', function () {
         return view('welcome');
     });
 });
